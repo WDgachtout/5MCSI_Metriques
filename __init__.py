69ef391f8ec4bf7589dd3a137428aa6fefca0304
WDgachtout <wdgachtout@gmail.com>
2025-04-01 14:11:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,9 @@ def mongraphique():
 def histogramme():
     return render_template("histogramme.html")
 
-
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+  
 if __name__ == "__main__":
   app.run(debug=True)
